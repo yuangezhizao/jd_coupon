677bbdf4a1464e7774d46b12a1eed4c49c7839f8
Zhang Jiawei <naivealv5@gmail.com>
2017-10-25 22:00:42 +0800

    update

--- jd_format.js
+++ jd_format.js
@@ -1,5 +1,5 @@
 //添加购物车
-http://cart.jd.com/gate.action?pid=1411676813&pcount=1&ptype=1
+https://cart.jd.com/gate.action?pid=1411676813&pcount=1&ptype=1
 
 //优惠券
 http://coupon.m.jd.com/coupons/show.action?
@@ -10,4 +10,15 @@ http://mkt.baitiao.jd.com/mobile/initMobile?
 http://mkt.baitiao.jd.com/activity/initParty?key=c358910d190f7a764477ab24660c0d07
 
 //微信抢购
-http://wqs.jd.com/order/s_confirm_miao.shtml?bid=&scene=jd&isCanEdit=1&EncryptInfo=&Token=&commlist=,,1,2256922
+http://wqs.jd.com/order/s_confirm_miao.shtml?bid=&scene=jd&isCanEdit=1&EncryptInfo=&Token=&commlist=,,1,4075581
+
+http://wqs.jd.com/order/s_confirm_miao.shtml?bid=&scene=jd&isCanEdit=1&EncryptInfo=&Token=&commlist=,,1,3484669
+
+//京豆兑换
+https://vip.jd.com/fuli/detail/750.html
+https://vip.m.jd.com/index.html?appName=fuli&id=750#detail
+
+https://p.m.jd.com/norder/order.action?wareId=10642770713&wareNum=1&enterOrder=true
+
+//领券api
+https://api.m.jd.com/client.action?functionId=newBabelAwardCollection&body={%22activityId%22:%2222Px9BahzVFBKsj3Fxz5GpXa4dKn%22,%22scene%22:%221%22,%22args%22:%22key=2bd58e1062924d7bbe1db0df9324ff1b,roleId=8709749,to=%22}&clientVersion=1.0.0&client=wh5&callback=jsonp19
